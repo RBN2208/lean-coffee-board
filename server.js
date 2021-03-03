c57b098df59599540eab5d3e1a893c9d90c4a094
Jeremias Erbs <jerry@badabam.de>
2021-03-03 11:58:11 +0100

    Install express and create simple server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer((req, res) => {
+const app = express()
+
+app.use((req, res) => {
   console.log(req.method, req.url)
   res.end('Hello world')
 })
 
-server.listen(3000, () => {
+
+app.listen(3000, () => {
   console.log('Server started at http://localhost:3000')
 })
