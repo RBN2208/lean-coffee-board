30072edd5c459f92d6ba11dae1159ba97ff5e923
Robin Steinhauser <rbn2208@gmail.com>
2021-03-03 15:00:21 +0100

    Added get and delete

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require('express')
 
 const app = express()
 
-app.use('/api/users', (req, res) => {
-    res.end('Users')
+let users = []
+
+const middleware = express.json()
+
+app.use(middleware)
+
+app.get('/api/users', (req, res) => {
+    res.json(users)
 })
 
-app.use('/api/cards', (req, res) => {
-    res.end('Cards')
+app.get('/api/users/:id', (req, res) => {
+    const {id} = req.params
+    res.json(users.find(user => user.id === id))
+})
+
+app.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params
+    const index = users.findIndex(user => user.id === id)
+    users = [...users.slice(0, index), ...users.slice(index +1)]
+    res.json(users.find(user => user.id === id))
+})
+
+app.post('/api/users', (req, res) => {
+    const newUser = {...req.body, id: uuidv4()}
+    users.push(newUser)
+    res.json(newUser)
+})
+
+app.get('/api/cards', (req, res) => {
+    res.json([{ title: "Frist card"}])
 })
 
 app.listen(3000, () => {
