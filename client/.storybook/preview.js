482370da88c85b12d0ba8938cfc177628f7b225a
Jeremias Erbs <jerry@badabam.de>
2021-03-08 14:09:58 +0100

    Set storybook default screen to mobile

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,7 +1,12 @@
 import GlobalStyle from '../src/components/GlobalStyle'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'iphone6',
+  },
 }
 
 export const decorators =[
